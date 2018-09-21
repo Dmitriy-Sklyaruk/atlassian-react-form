8df23608d40ac2f698691dc957489d18794b772e
Dmytro Skliaruk <sklyaruk97@gmail.com>
2018-09-21 19:09:25 +0300

    Added simple test for question form

--- src/__tests__/question-test.js
+++ src/__tests__/question-test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import QuestionForm from '../components/question';
 
+
 import renderer from 'react-test-renderer';
+
+import { configure } from 'enzyme';
+
+import {shallow, mount} from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 
 test('renders correctly', () => {
   const tree = renderer.create(<QuestionForm />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+
+test('<QuestionForm/>', () => {
+  it('renders 3 Fields', () => {
+    const wrapper = mount(<QuestionForm/>);
+    expect(wrapper.find('Field').length).toBe(3);
+  });
 });
