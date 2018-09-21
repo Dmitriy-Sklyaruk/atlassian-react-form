b50594f862315538a393e029a21ec2b06ce5043c
Dmytro Skliaruk <dskli@softserveinc.com>
2018-09-21 16:03:50 +0300

    Added simple question & registration form

---
+++ src/components/question.jsx
@@ -0,0 +1,114 @@
+// @flow
+import React, {PureComponent} from 'react';
+import SingleSelect from '@atlaskit/single-select';
+import FieldText from '@atlaskit/field-text';
+import FieldTextArea from '@atlaskit/field-text-area';
+import Button from '@atlaskit/button';
+import {Checkbox} from '@atlaskit/checkbox';
+import {Link} from 'react-router';
+
+import './style.css'
+
+import Form, {
+  FormHeader,
+  FormSection,
+  FormFooter,
+  Field,
+  FieldGroup,
+  Validator
+} from '@atlaskit/form';
+
+
+const selectItems = [
+  {
+    heading: 'PLAN, TRACK, SUPPORT',
+    items: [
+      { content: 'Jira Software', value: 'jira_software' },
+      { content: 'Jira Service Desk', value: 'jira_service_desk' },
+      { content: 'Jira Core', value: 'jira_core' },
+      { content: 'Portfolio For Jira', value: 'jira_portfolio' },
+    ],
+  },
+  {
+    heading: 'CODE, BUILD, SHIP',
+    items: [
+      { content: 'Bitbucket', value: 'bitbucket' },
+      { content: 'Sourcetree', value: 'sourcetree' },
+      { content: 'Bamboo', value: 'bamboo' },
+      { content: 'Fisheye', value: 'fisheye' },
+      { content: 'Cruicible', value: 'cruicible' },
+    ],
+  },
+  {
+    items: [{ content: 'I\'m not sure which product are right for me', value: 'default' }],
+  },
+];
+
+
+const isEmailValid = (value : string): boolean => {
+  return value.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i);
+};
+
+class QuestionForm extends PureComponent<void> {
+
+  formRef: any;
+
+  onSubmitHandler = () => {
+    console.log('onSubmitHandler');
+
+    const validateResult = this.formRef.validate();
+    console.log(validateResult);
+
+    if (validateResult.isInvalid) {
+      console.log('onSubmitHandler = Form Fields Invalid');
+    } else {
+      // Now call submit when your done
+      this.formRef.submit();
+    }
+  };
+
+  render() {
+    return (<div className="form">
+      <Form name="create-repo" onSubmit={this.onSubmitHandler} onReset={this.onResetHandler} ref={form => {
+          this.formRef = form;
+        }}
+          onValidate={this.onValidateHandler} action="//httpbin.org/get" method="GET" target="submitFrame">
+        <FormHeader title="Have a question for us?" position="center"/>
+
+        <FormSection label="Email">
+          <Field label="Email" isRequired="isRequired" validators={[<Validator func={isEmailValid} invalid="Email is invalid"/>]}>
+            <FieldText name="email" placeholder="name@company.com" shouldFitContainer="shouldFitContainer"/>
+          </Field>
+          <Field >
+            <SingleSelect label="Which product would you like advice on?" items={selectItems} placeholder="Choose a product" name="product" shouldFitContainer/>
+          </Field>
+
+          <FieldTextArea label="Your question" name="question" shouldFitContainer="shouldFitContainer" enableResize="true" minimumRows="7"/>
+        </FormSection>
+
+        <FormFooter actionsContent={[{
+              id: 'submit-button'
+            }
+          ]}>
+          <Button appearance="primary" type="submit">
+            Send
+          </Button>
+        </FormFooter>
+      </Form>
+
+      <p>Do you want to Sign Up? Click
+        <Link to="/registration"> here</Link>
+      </p>
+      <p>The data submitted by the form will appear below:</p>
+      <iframe title="Response Frame" id="submitFrame" name="submitFrame" style={{
+          width: '100%',
+          height: '400px',
+          borderStyle: 'dashed',
+          borderWidth: '1px',
+          color: '#ccc'
+        }}/>
+    </div>);
+  }
+}
+
+export default QuestionForm;
---
+++ src/components/registration.jsx
@@ -0,0 +1,87 @@
+import React, {PureComponent} from 'react';
+import FieldText from '@atlaskit/field-text';
+import Button from '@atlaskit/button';
+import {Checkbox} from '@atlaskit/checkbox';
+
+import './style.css'
+
+import Form, {FormHeader, FormSection, FormFooter, Field, Validator} from '@atlaskit/form';
+
+//TODO: Should be refactored/moved to a separate place.
+const isEmailValid = (value : string): boolean => {
+  return value.match(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i);
+};
+
+const isPasswordValid = (value : string): boolean => {
+  return value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/i);
+};
+
+class RegistrationForm extends PureComponent<void> {
+
+  onSubmitHandler = () => {
+    console.log('Submitting form');
+
+    const validateResult = this.formRef.validate();
+    console.log(validateResult);
+
+    if (validateResult.isInvalid) {
+      console.log('onSubmitHandler = Form Fields Invalid');
+    } else {
+      this.formRef.submit();
+    }
+  };
+
+  render() {
+    return (<div className="form">
+      <Form name="registration-form" onSubmit={this.onSubmitHandler} onReset={this.onResetHandler} ref={form => {
+          this.formRef = form;
+        }} method="GET" target="submitFrame" action="//httpbin.org/get">
+
+        <FormHeader title="Please, fill fields below to sign up"/>
+
+        <FormSection>
+          <Field label="First Name" isRequired="isRequired">
+            <FieldText name="first_name" placeholder="First Name" shouldFitContainer="shouldFitContainer"/>
+          </Field>
+          <Field label="Last Name" isRequired="isRequired">
+            <FieldText name="last_name" placeholder="Last Name" shouldFitContainer="shouldFitContainer"/>
+          </Field>
+          <Field label="Email" isRequired="isRequired" validators={[<Validator func={isEmailValid} invalid="Email is invalid"/>]}>
+            <FieldText name="email" placeholder="email@domain.com" shouldFitContainer="shouldFitContainer"/>
+          </Field>
+
+          <Field label="Password" validators={[<Validator func={isPasswordValid} invalid="Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>]}>
+            <FieldText name="password" type="password" shouldFitContainer="shouldFitContainer"/>
+          </Field>
+          <p>Subscriptions</p>
+          <span>
+            <Checkbox label="Send me news to email" value="news" name="subscriptions"/>
+            <Checkbox label="I want to get latest updates" value="updates" name="subscriptions"/>
+          </span>
+        </FormSection>
+
+        <FormFooter actionsContent={[
+            {
+              id: 'submit-button'
+            }, {}
+          ]}>
+          <Button appearance="primary" type="submit">
+            Sign Up
+          </Button>
+          <Button appearance="subtle" href="/question">Back</Button>
+        </FormFooter>
+      </Form>
+
+      <p>The data submitted by the form will appear below:</p>
+      <iframe title="Response Frame" id="submitFrame" name="submitFrame" style={{
+          width: '100%',
+          height: '400px',
+          borderStyle: 'dashed',
+          borderWidth: '1px',
+          color: '#ccc'
+        }}/>
+    </div>);
+  }
+}
+
+export default RegistrationForm;
